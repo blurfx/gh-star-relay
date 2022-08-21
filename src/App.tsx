60b5ff04011116d53b87c39dff07622e445cfe16
changhui lee <iam@xo.dev>
2022-08-21 15:15:52 +0900

    feat: 검색 폼 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 
 import SearchResult from './components/SearchResult';
 
 const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null);
+  const onSubmit: React.FormEventHandler = (e) => {
+    e.preventDefault();
+    setQuery(inputRef?.current?.value ?? '');
+  };
+
   return (
     <div>
       <header className='App-header'>
-        <SearchResult query={'그린랩스'} />
+        <form onSubmit={onSubmit}>
+          <input type='text' name={'query'} ref={inputRef} />
+          <button type={'submit'}>검색</button>
+        </form>
+        <React.Suspense fallback={'Loading...'}>
+          { query && <SearchResult query={query} /> }
+        </React.Suspense>
       </header>
     </div>
   );
